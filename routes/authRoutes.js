65ea1fad330900b58b08369c6c03fb36e4f667c6
Dan Teixeira <danrleyt@gmail.com>
2019-11-05 14:40:35 -0300

    fixing proxy

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,13 @@ module.exports = app => {
     })
   );
 
-  app.get('/auth/spotify/callback', passport.authenticate('spotify'));
+  app.get(
+    '/auth/spotify/callback',
+    passport.authenticate('spotify'),
+    (req, res) => {
+      res.send(req.user);
+    }
+  );
 
   app.get('/api/logout', (req, res) => {
     req.logout();
